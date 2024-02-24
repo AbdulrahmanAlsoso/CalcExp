e1414146a65bafc96f3e7a3c7f20edeb8976386e
SEng.Abdulrahman <abdoannpk@gmail.com>
2024-02-24 12:43:36 +0300

    Inital Commit

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulcExp.Data
+{
+    public class AppDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                .Build();
+            var constr = config.GetSection("constr").Value;
+            optionsBuilder.UseSqlServer(constr);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System ;
+namespace CulcExp{
+    public class Program{
+        public static void Main(){
+            Console.WriteLine("Hello World");
+        }
+    }
+}
